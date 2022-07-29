aa0f3239982d084ab24f0b27cbdc44fc704879ae
msm <msmasd@naver.com>
2022-07-29 20:16:41 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/linesteams/linesapiserver/LinesApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.linesteams.linesapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinesApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinesApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linesteams/linesapiserver/LinesApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linesteams.linesapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinesApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
